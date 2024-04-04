793168efa9789eda96e8be42344ce3733516b0ba
GytenisBorusas <gytenis.borusas@gmail.com>
2024-04-04 13:02:12 -0600

    start of the project

---
+++ insert.sql
@@ -0,0 +1,62 @@
+-- INSERT INTO  cats
+-- 	(name, age)
+-- VALUES 
+-- 	("Jerry", 2);
+--     
+
+
+-- SELECT * FROM people;
+
+
+-- INSERT INTO  cats
+-- 	(name, age)
+-- VALUES 
+-- 	("Suzzy", 2),
+--     ("Charlie", 4),
+--     ("Lazy Bear", 9);
+--     
+
+
+-- CREATE TABLE people (
+-- 		first_name VARCHAR(20), 
+-- 		last_name VARCHAR(20),
+-- 		age INT
+--         );
+    
+    
+-- INSERT INTO  people
+-- 		(first_name, last_name, age)
+-- VALUES 
+-- 		("Linda", "Belcher", 45),
+--     	("Philip", "Frond", 38),
+--     	("Calvin", "Fischoeder", 70);
+--     
+    
+    
+    
+-- CREATE TABLE cats2
+-- (
+-- 	name VARCHAR(20) NOT NULL,
+--     age INT NOT NULL
+-- );
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
