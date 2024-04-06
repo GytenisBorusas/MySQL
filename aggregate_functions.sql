247fb7a00353a50c2e297ac0ed5ba8e0e59eff0c
GytenisBorusas <gytenis.borusas@gmail.com>
2024-04-06 10:05:57 -0600

    complete aggregate functions

--- aggregate_functions.sql
+++ aggregate_functions.sql
@@ -9,6 +9,16 @@
 
 
 
+-- SELECT author_lname, COUNT(*) 
+-- FROM books
+-- GROUP BY author_lname;
+--  
+-- SELECT 
+--     author_lname, COUNT(*) AS books_written
+-- FROM
+--     books
+-- GROUP BY author_lname
+-- ORDER BY books_written DESC;
 
 
 
@@ -16,42 +26,102 @@
 
 
 
+-- SELECT MAX(pages) FROM books;
+--  
+-- SELECT MIN(author_lname) FROM books;
 
 
 
 
 
+-- SELECT title, pages FROM books
+-- WHERE pages = (SELECT MAX(pages) FROM books);
+--  
+-- SELECT MIN(released_year) FROM books;
+--  
+-- SELECT title, released_year FROM books 
+-- WHERE released_year = (SELECT MIN(released_year) FROM books);
 
 
 
 
 
+-- SELECT author_fname, author_lname, COUNT(*) 
+-- FROM books 
+-- GROUP BY author_lname, author_fname;
+--  
+--  
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS author,  COUNT(*)
+-- FROM books
+-- GROUP BY author;
 
 
 
 
 
+-- SELECT author_lname, MIN(released_year) FROM books GROUP BY author_lname;
+--  
+-- SELECT author_lname, MAX(released_year), MIN(released_year) FROM books GROUP BY author_lname;
+--  
+-- SELECT 
+-- 	author_lname, 
+-- 	COUNT(*) as books_written, 
+-- 	MAX(released_year) AS latest_release,
+-- 	MIN(released_year)  AS earliest_release,
+--       MAX(pages) AS longest_page_count
+-- FROM books GROUP BY author_lname;
+--  
+--  
+-- SELECT 
+-- 	author_lname, 
+--         author_fname,
+-- 	COUNT(*) as books_written, 
+-- 	MAX(released_year) AS latest_release,
+-- 	MIN(released_year)  AS earliest_release
+-- FROM books GROUP BY author_lname, author_fname;
 
 
 
 
 
+-- SELECT SUM(pages) FROM books;
+--  
+--  
+-- SELECT author_lname, COUNT(*), SUM(pages)
+-- FROM books
+-- GROUP BY author_lname;
 
 
 
 
 
+-- SELECT AVG(pages) FROM books;
+--  
+-- SELECT AVG(released_year) FROM books;
+--  
+-- SELECT 
+--     released_year, 
+--     AVG(stock_quantity), 
+--     COUNT(*) FROM books
+-- GROUP BY released_year;
 
 
 
 
 
-
-
-
-
-
-
+-- SELECT COUNT(title) FROM books;
+-- SELECT released_year, COUNT(title) FROM books GROUP BY released_year;
+-- SELECT SUM(stock_quantity) FROM books;
+-- SELECT author_lname, author_fname, AVG(released_year) FROM books GROUP BY author_lname, author_fname;
+-- SELECT CONCAT(author_fname, ' ', author_lname) AS Author, Pages FROM books WHERE pages = (SELECT MAX(pages) FROM books);
+-- SELECT * FROM books;
+-- SELECT 
+-- 	released_year AS year, 
+--     COUNT(released_year) AS '# books', 
+--     AVG(pages) as 'avg pages' 
+-- FROM books 
+-- GROUP BY released_year 
+-- ORDER BY released_year ASC;
 
 
 
