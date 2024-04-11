e341ba59165751fee4c2a2005900d0be57bc1d2b
GytenisBorusas <gytenis.borusas@gmail.com>
2024-04-11 14:54:51 -0600

    complete joins

--- joins.sql
+++ joins.sql
@@ -123,54 +123,115 @@
 
 
 
-SELECT 
-    first_name, last_name, order_date, amount
-FROM
-    customers
-        RIGHT JOIN
-    orders ON customers.id = orders.customer_id;
+-- SELECT 
+--     first_name, last_name, order_date, amount
+-- FROM
+--     customers
+--         RIGHT JOIN
+--     orders ON customers.id = orders.customer_id;
+
+-- DROP database shop
+-- DROP TABLE customers;
+-- DROP TABLE orders;
+
+-- CREATE DATABASE shop
+
+-- CREATE TABLE customers (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     email VARCHAR(50)
+-- );
+--  
 
 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- CREATE TABLE orders (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     order_date DATE,
+--     amount DECIMAL(8 , 2 ),
+--     customer_id INT,
+--     FOREIGN KEY (customer_id)
+--         REFERENCES customers (id)
+--         ON DELETE CASCADE
+-- );
 
 
 
 
 
 
+-- CREATE TABLE students (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name VARCHAR(50)
+-- );
+
+-- CREATE TABLE papers (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(50),
+--     grade INT,
+--     student_id INT,
+--     FOREIGN KEY (student_id) REFERENCES students(id)
+-- );
+
+
+-- INSERT INTO students (first_name) VALUES 
+-- ('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+--  
+-- INSERT INTO papers (student_id, title, grade ) VALUES
+-- (1, 'My First Book Report', 60),
+-- (1, 'My Second Book Report', 75),
+-- (2, 'Russian Lit Through The Ages', 94),
+-- (2, 'De Montaigne and The Art of The Essay', 98),
+-- (4, 'Borges and Magical Realism', 89);
+
+
+
+-- SELECT * FROM papers;
+
+-- SELECT 
+--     first_name, title, grade
+-- FROM
+--     (students
+--         RIGHT JOIN
+--     papers ON students.id = papers.student_id)
+-- ORDER BY grade DESC;
+
+-- SELECT 
+--     first_name, IFNULL(title, 'MISSING'), IFNULL(grade, 0)
+-- FROM
+--     (students
+--         LEFT JOIN
+--     papers ON students.id = papers.student_id);
+
+
+
+
+-- SELECT 
+--     first_name, IFNULL(AVG(grade), 0) AS 'average'
+-- FROM
+--     students
+--         LEFT JOIN
+--     papers ON students.id = papers.student_id
+-- GROUP BY students.first_name
+-- ORDER BY Average DESC;
 
 
 
 
 
-
-
-
+-- SELECT 
+--     first_name,
+--     IFNULL(AVG(grade), 0) AS 'average',
+--     CASE
+--         WHEN IFNULL(AVG(grade), 0) >= 75 THEN 'PASSING'
+--         ELSE 'FAILING'
+--     END AS 'passing_status'
+-- FROM
+--     students
+--         LEFT JOIN
+--     papers ON students.id = papers.student_id
+-- GROUP BY students.first_name
+-- ORDER BY average DESC;
 
 
 
